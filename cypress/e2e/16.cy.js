39eadc0f1805f66d6a2c93dd53ff7a3a0cd41199
Artūras Jevtuchovas <artjev28@gmail.com>
2025-06-13 17:29:02 +0300

    Update 16.cy.js

--- cypress/e2e/16.cy.js
+++ cypress/e2e/16.cy.js
@@ -18,7 +18,5 @@ before(() => {
 
 it('Test Case 16: Place Order: Login before Checkout', () => {
 
-    //useless task to bring more confusion rather than actual testing experience.
-    cy.log('SKIPPING IRRELEVANT TEST')
     
 });
